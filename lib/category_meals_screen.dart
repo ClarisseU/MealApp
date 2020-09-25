83110db2e6b8bf6dc200ab1a39d100408d4f30fb
unknown <clarisseum@gmail.com>
2020-09-25 14:42:58 +0200

    add navigator

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('The Receipes'),
+      ),
+      body: Center(
+        child: Text(
+          'The Receipes for the Category!',
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -10,28 +10,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var themeData = ThemeData(
-            primarySwatch: Colors.pink,
-            accentColor: Colors.amber,
-            canvasColor: Color.fromRGBO(255, 254, 229, 1),
-            fontFamily: 'Raleway',
-            textTheme: ThemeData.light().textTheme.copyWith(
-              body1: TextStyle(
-                color: Color.fromRGBO(20, 51, 51, 1),
-              )),
-              // body2: TextStyle(
-              //   color: Color.fromRGBO(20, 51, 51, 1),
-              // ),
-              // title: TextStyle(
-              //   fontSize: 24,
-              //   fontFamily: 'RobotoCondensed'
-              // ),
-              );
-        return MaterialApp(
-          title: 'DeliMeals',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          home: CategoriesScreen(),
-      );
+      primarySwatch: Colors.pink,
+      accentColor: Colors.amber,
+      canvasColor: Color.fromRGBO(255, 254, 229, 1),
+      fontFamily: 'Raleway',
+      textTheme: ThemeData.light().textTheme.copyWith(
+            body1: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1),
+          )),
+      // body2: TextStyle(
+      //   color: Color.fromRGBO(20, 51, 51, 1),
+      // ),
+      // title: TextStyle(
+      //   fontSize: 20,
+      //   fontFamily: 'RobotoCondensed',
+      //   fontWeight: Bold,
+      // ),
+    );
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CategoriesScreen(),
+    );
   }
 }
